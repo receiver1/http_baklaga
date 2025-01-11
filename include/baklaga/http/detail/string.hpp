3ec4f9cea787d2745025337e95837336819e7e94
kelarisss <143023834+annihilatorq@users.noreply.github.com>
2025-01-11 14:38:14 +0100

    implement `to_arithmetic` for enums too

--- include/baklaga/http/detail/string.hpp
+++ include/baklaga/http/detail/string.hpp
@@ -34,23 +34,29 @@ struct convert_result_t {
   std::error_code ec;
 };
 
-template <typename T, typename DecayedT = std::decay_t<T>>
-  requires(std::is_arithmetic_v<DecayedT>)
-[[nodiscard]] constexpr auto to_arithmetic(std::ranges::sized_range auto buffer,
-                                           const int base = 10) noexcept {
-  convert_result_t<DecayedT> result{};
-  std::from_chars(buffer.data(), buffer.data() + buffer.size(), result.value, base);
-  return result;
-}
-
 template <typename T, typename DecayedT = std::decay_t<T>,
-          typename UnderlyingT = std::underlying_type_t<DecayedT>>
-  requires(std::is_arithmetic_v<DecayedT> || std::is_enum_v<DecayedT>)
+          bool IsEnum = std::is_enum_v<DecayedT>>
+  requires(std::is_arithmetic_v<DecayedT> || IsEnum)
 [[nodiscard]] constexpr auto to_arithmetic(std::ranges::sized_range auto buffer,
                                            const int base = 10) noexcept {
-  convert_result_t<DecayedT> result{};
-  std::from_chars(buffer.data(), buffer.data() + buffer.size(), reinterpret_cast<UnderlyingT&>(result.value), base);
-  return result;
+  using result_t = convert_result_t<DecayedT>;
+
+  // std::conditional_t with std::underlying_type_t won't work here.
+  // conditional_t is a meta function analogue to the regular if,
+  // this means that all if paths must be ct evaluated
+  // (fun fact: all paths are fully ct evaluated, but it still doesn't work)
+  if constexpr (IsEnum) {
+    using underlying_t = std::underlying_type_t<DecayedT>;
+    underlying_t value{};
+    auto [_, ec] = std::from_chars(buffer.data(), buffer.data() + buffer.size(),
+                                   value, base);
+    return result_t{static_cast<DecayedT>(value), std::make_error_code(ec)};
+  } else {
+    DecayedT value{};
+    auto [_, ec] = std::from_chars(buffer.data(), buffer.data() + buffer.size(),
+                                   value, base);
+    return result_t{value, std::make_error_code(ec)};
+  }
 }
 
 }  // namespace baklaga::http::detail
